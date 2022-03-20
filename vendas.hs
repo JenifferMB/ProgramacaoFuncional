52a753d08e8731f9967049bcfa182931f292a3d7
Jeniffer Borges <jenifferrborges@gmail.coms>
2022-03-20 20:04:59 -0300

    teste

--- vendas.hs
+++ vendas.hs
@@ -7,3 +7,5 @@ totalVendas :: Int -> Int
 totalVendas n | n == 0 = 0 -- Nao ha lacos de repeticao, se usa funcao recursiva 
               | n > 0 = vendas n + totalVendas (n -1)
               | otherwise = 0
+
+              -- teste
